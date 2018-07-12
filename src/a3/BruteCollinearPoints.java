aad9f7fc2486779ade3e8caaf0321df2495569f3
fras2560 <dallas.fraser.waterloo@gmail.com>
2018-07-12 08:52:10 -0400

    Few bug fixes and a unit test

--- src/a3/BruteCollinearPoints.java
+++ src/a3/BruteCollinearPoints.java
@@ -20,66 +20,25 @@ public class BruteCollinearPoints {
                         s3 = points[p1].slopeTo(points[p4]);
                         if(s1 == s2 && s1 == s3) {
                             if(this.numberSegments == this.lines.length) {
-                                this.resize(this.numberSegments * 2);
+                                this.lines = Helper.resize(this.lines, this.numberSegments * 2);
                             }
-                            this.lines[this.numberSegments] = new LineSegment(this.smallestPoint(points[p1],
-                                                                                                 points[p2],
-                                                                                                 points[p3],
-                                                                                                 points[p4]),
-                                                                              this.largestPoint(points[p1],
-                                                                                                points[p2],
-                                                                                                points[p3],
-                                                                                                points[p4])
+                            this.lines[this.numberSegments] = new LineSegment(Helper.smallestPoint(points[p1],
+                                                                                                   points[p2],
+                                                                                                   points[p3],
+                                                                                                   points[p4]),
+                                                                              Helper.largestPoint(points[p1],
+                                                                                                  points[p2],
+                                                                                                  points[p3],
+                                                                                                  points[p4])
                                                                               );
                             this.numberSegments += 1;
                         }
+                        
                     }
                 }
             }
         }
-    }
-
-    private Point smallestPoint(Point p1, Point p2, Point p3, Point p4) {
-        Point smallest;
-        if(p1.compareTo(p2) <= 0 && p1.compareTo(p3) <= 0 && p1.compareTo(p4) <= 0) {
-            smallest = p1;
-        } else if (p2.compareTo(p3) <= 0 && p2.compareTo(p4) <= 0) {
-            smallest = p2;
-        } else if (p3.compareTo(p4) <= 0) {
-            smallest = p3;
-        } else {
-            smallest = p4;
-        }
-        return smallest;
-    }
-
-    private Point largestPoint(Point p1, Point p2, Point p3, Point p4) {
-        Point largest;
-        if(p1.compareTo(p2) <= 0 && p1.compareTo(p3) <= 0 && p1.compareTo(p4) <= 0) {
-            largest = p1;
-        } else if (p2.compareTo(p3) <= 0 && p2.compareTo(p4) <= 0) {
-            largest = p2;
-        } else if (p3.compareTo(p4) <= 0) {
-            largest = p3;
-        } else {
-            largest = p4;
-        }
-        return largest;
-    }
-
-    private void resize(int capacity) {
-        LineSegment[] copy = new LineSegment[capacity];
-        int pos = 0;
-        int copyPos = 0;
-        while (pos < this.lines.length) {
-            if (this.lines[pos] != null) {
-                copy[copyPos] = this.lines[pos];
-                copyPos++;
-            }
-            pos++;
-            
-        }
-        this.lines = copy;
+        this.lines = Helper.resize(this.lines, this.numberSegments);
     }
 
     public int numberOfSegments() {
