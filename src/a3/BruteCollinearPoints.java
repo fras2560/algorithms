2ea045a1a7cb6442e2a924a62ca7075e9c8eefed
fras2560 <dallas_j_frase@hotmail.com>
2018-07-14 22:14:03 -0400

    Removed helper since princeton doesnt like extra files
    	- replaced tabs with spaces

--- src/a3/BruteCollinearPoints.java
+++ src/a3/BruteCollinearPoints.java
@@ -23,13 +23,13 @@ public class BruteCollinearPoints {
                         s3 = points[p1].slopeTo(points[p4]);
                         if(s1 == s2 && s1 == s3) {
                             if(this.numberSegments == this.lines.length) {
-                                this.lines = Helper.resize(this.lines, this.numberSegments * 2);
+                                this.lines = BruteCollinearPoints.resize(this.lines, this.numberSegments * 2);
                             }
-                            this.lines[this.numberSegments] = new LineSegment(Helper.smallestPoint(points[p1],
+                            this.lines[this.numberSegments] = new LineSegment(BruteCollinearPoints.smallestPoint(points[p1],
                                                                                                    points[p2],
                                                                                                    points[p3],
                                                                                                    points[p4]),
-                                                                              Helper.largestPoint(points[p1],
+                                                                              BruteCollinearPoints.largestPoint(points[p1],
                                                                                                   points[p2],
                                                                                                   points[p3],
                                                                                                   points[p4])
@@ -41,7 +41,7 @@ public class BruteCollinearPoints {
                 }
             }
         }
-        this.lines = Helper.resize(this.lines, this.numberSegments);
+        this.lines = BruteCollinearPoints.resize(this.lines, this.numberSegments);
     }
 
     public int numberOfSegments() {
@@ -50,6 +50,49 @@ public class BruteCollinearPoints {
 
     public LineSegment[] segments() {
         return this.lines;
+    }
+
+    private static Point smallestPoint(Point p1, Point p2, Point p3, Point p4) {
+        Point smallest;
+        if(p1.compareTo(p2) <= 0 && p1.compareTo(p3) <= 0 && p1.compareTo(p4) <= 0) {
+            smallest = p1;
+        } else if (p2.compareTo(p3) <= 0 && p2.compareTo(p4) <= 0) {
+            smallest = p2;
+        } else if (p3.compareTo(p4) <= 0) {
+            smallest = p3;
+        } else {
+            smallest = p4;
+        }
+        return smallest;
+    }
+
+    private static Point largestPoint(Point p1, Point p2, Point p3, Point p4) {
+        Point largest;
+        if(p1.compareTo(p2) >= 0 && p1.compareTo(p3) >= 0 && p1.compareTo(p4) >= 0) {
+            largest = p1;
+        } else if (p2.compareTo(p3) >= 0 && p2.compareTo(p4) >= 0) {
+            largest = p2;
+        } else if (p3.compareTo(p4) >= 0) {
+            largest = p3;
+        } else {
+            largest = p4;
+        }
+        return largest;
+    }
+
+    private static LineSegment[] resize(LineSegment[] array, int capacity) {
+        LineSegment[] copy = new LineSegment[capacity];
+        int pos = 0;
+        int copyPos = 0;
+        while (pos < array.length) {
+            if (array[pos] != null) {
+                copy[copyPos] = array[pos];
+                copyPos++;
+            }
+            pos++;
+            
+        }
+        return copy;
     }
 
     public static void main(String[] args) {
--- src/a3/FastCollinearPoints.java
+++ src/a3/FastCollinearPoints.java
@@ -7,88 +7,101 @@ import java.util.Arrays;
 public class FastCollinearPoints {
     private LineSegment[] lines;
     private int numberSegments;
-	public FastCollinearPoints(Point[] points) {
-		this.lines = new LineSegment[points.length];
-		for (int i = 0; i < points.length; i++) {
-			Point origin = points[i];
-			Arrays.sort(points, i+1, points.length, origin.slopeOrder());
-			this.pullLines(origin, points, i+1);
-		}
-        this.lines = Helper.resize(this.lines, this.numberSegments);
-	}
+    public FastCollinearPoints(Point[] points) {
+        this.lines = new LineSegment[points.length];
+        for (int i = 0; i < points.length; i++) {
+            Point origin = points[i];
+            Arrays.sort(points, i+1, points.length, origin.slopeOrder());
+            this.pullLines(origin, points, i+1);
+        }
+        this.lines = FastCollinearPoints.resize(this.lines, this.numberSegments);
+    }
 
-	private void pullLines(Point origin, Point[] points, int i) {
-		// now check values
-		double prev = 0;
-		int count = 1;
-		Point smallest = origin;
-		Point largest = origin;
-		while (i < points.length) {
-			System.out.println("Point:" + points[i] + " Origin:" + origin + " Smallest:" + smallest + " Largest:" + largest + " prev:" + prev + " count:" + count);
-			if (origin.slopeTo(points[i]) == prev) {
-				if (points[i].compareTo(smallest) <= 0) {
-					smallest = points[i];
-				}
-				if (points[i].compareTo(largest) >= 0) {
-					largest = points[i];
-				}
-				count += 1;
-			} else {
-				if(count >= 3) {
-					if(this.numberSegments == this.lines.length) {
-                        this.lines = Helper.resize(this.lines, this.numberSegments * 2);
+    private void pullLines(Point origin, Point[] points, int i) {
+        // now check values
+        double prev = 0;
+        int count = 1;
+        Point smallest = origin;
+        Point largest = origin;
+        while (i < points.length) {
+            if (origin.slopeTo(points[i]) == prev) {
+                if (points[i].compareTo(smallest) <= 0) {
+                    smallest = points[i];
+                }
+                if (points[i].compareTo(largest) >= 0) {
+                    largest = points[i];
+                }
+                count += 1;
+            } else {
+                if(count >= 3) {
+                    if(this.numberSegments == this.lines.length) {
+                        this.lines = FastCollinearPoints.resize(this.lines, this.numberSegments * 2);
                     }
                     this.lines[this.numberSegments] = new LineSegment(smallest, largest);
                     this.numberSegments += 1;
-				}
-				count = 1;
-				smallest = points[i].compareTo(origin) <= 0 ? points[i] : origin;
-				largest = points[i].compareTo(origin) >= 0 ? points[i] : origin;
-				prev = origin.slopeTo(points[i]);
-			}
-			i++;
-		}
-		if (count >= 3) {
-			if(this.numberSegments == this.lines.length) {
-                this.lines = Helper.resize(this.lines, this.numberSegments * 2);
+                }
+                count = 1;
+                smallest = points[i].compareTo(origin) <= 0 ? points[i] : origin;
+                largest = points[i].compareTo(origin) >= 0 ? points[i] : origin;
+                prev = origin.slopeTo(points[i]);
+            }
+            i++;
+        }
+        if (count >= 3) {
+            if(this.numberSegments == this.lines.length) {
+                this.lines = FastCollinearPoints.resize(this.lines, this.numberSegments * 2);
             }
             this.lines[this.numberSegments] = new LineSegment(smallest, largest);
             this.numberSegments += 1;
-		}
-		System.out.println();
-	}
-	public int numberOfSegments() {
-		return this.numberSegments;
-	}
+        }
+    }
+    public int numberOfSegments() {
+        return this.numberSegments;
+    }
 
-	public LineSegment[] segments() {
-		return this.lines;
-	}
+    public LineSegment[] segments() {
+        return this.lines;
+    }
 
-	public static void main(String[] args) {
-	    // read the n points from a file
-	    In in = new In(args[0]);
-	    int n = in.readInt();
-	    Point[] points = new Point[n];
-	    for (int i = 0; i < n; i++) {
-	        int x = in.readInt();
-	        int y = in.readInt();
-	        points[i] = new Point(x, y);
-	    }
-	    // draw the points
-	    StdDraw.enableDoubleBuffering();
-	    StdDraw.setXscale(0, 32768);
-	    StdDraw.setYscale(0, 32768);
-	    for (Point p : points) {
-	        p.draw();
-	    }
-	    StdDraw.show();
-	    // print and draw the line segments
-	    FastCollinearPoints collinear = new FastCollinearPoints(points);
-	    for (LineSegment segment : collinear.segments()) {
-	        StdOut.println(segment);
-	        segment.draw();
-	    }
-	    StdDraw.show();
-	}
+    public static LineSegment[] resize(LineSegment[] array, int capacity) {
+        LineSegment[] copy = new LineSegment[capacity];
+        int pos = 0;
+        int copyPos = 0;
+        while (pos < array.length) {
+            if (array[pos] != null) {
+                copy[copyPos] = array[pos];
+                copyPos++;
+            }
+            pos++;
+            
+        }
+        return copy;
+    }
+
+    public static void main(String[] args) {
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+        // print and draw the line segments
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+    }
 }
