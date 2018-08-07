c36afa99060afd214a53abffad0c277a94ac627e
fras2560 <dallas.fraser.waterloo@gmail.com>
2018-08-07 11:31:19 -0400

    Added javadoc and spacing

--- src/a1/Percolation.java
+++ src/a1/Percolation.java
@@ -1,3 +1,12 @@
+/******************************************************************************
+ *  Compilation:  javac -cp  "alg4s.jar" Percolation.java
+ *  Execution:    java Percolation
+ *  Dependencies: algs4
+ *  Author: Dallas Fraser
+ *  A data structure to see if something percolates.
+ *  For use on Coursera, Algorithms Part I programming assignment.
+ *
+ ******************************************************************************/
 package a1;
 
 import edu.princeton.cs.algs4.StdOut;
@@ -9,102 +18,182 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
  * @since 2018-06-06
  */
 public class Percolation {
-    private final WeightedQuickUnionUF connections;
-    private boolean[]sites;
-    private final int n;
-    private final int top;
-    private final int bottom;
-    private int openedSites;
-    public Percolation(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException();
-        }
-        this.sites = new boolean[n*n+2];
-        for (int index = 0; index <= (n*n) + 1; index++) {
-            this.sites[index] = false;
-        }
-        this.n = n;
-        this.connections = new WeightedQuickUnionUF(n*n+2);
-        this.top = 0;
-        this.bottom = n*n + 1;
-        this.sites[top] = true;
-        this.sites[bottom] = true;
-        this.openedSites = 0;
-    }
+	private final WeightedQuickUnionUF connections;
+	private final int n;
+	private final int top;
+	private final int bottom;
+	private boolean[] sites;
+	private int openedSites;
 
-    private int rowColTransposed(int row, int col) {
-        if (row <= 0 || row > this.n || col <= 0  || col > this.n) {
-            throw new IllegalArgumentException();
-        }
-        int index = (col) + this.n * (row - 1);
-        return index;
-    }
+	/**
+	 * The default constructor that creates an n by n square
+	 *
+	 * @param n
+	 *            the size of n
+	 */
+	public Percolation(int n) {
+		if (n <= 0) {
+			throw new IllegalArgumentException();
+		}
 
-    private void connect(int index1, int index2) {
-        if (this.sites[index1] && this.sites[index2]) {
-            this.connections.union(index1, index2);
-        }
-    }
+		this.sites = new boolean[n*n+2];
+		for (int index = 0; index <= (n*n) + 1; index++) {
+			this.sites[index] = false;
+		}
 
-    public void open(int row, int col) {
-        this.openedSites ++;
-        int index = this.rowColTransposed(row, col);
-        this.sites[index] = true;
-        // do up and above first
-        if (row == 1 ) {
-            this.connect(this.top, index);
-            this.connect(index, this.rowColTransposed(row + 1, col));
-        } else if (row == this.n) {
-            this.connect(this.bottom, index);
-            this.connect(index, this.rowColTransposed(row - 1, col));
-        } else {
-            this.connect(index, this.rowColTransposed(row - 1, col));
-            this.connect(index, this.rowColTransposed(row + 1, col));
-        }
-        if (col > 1) {
-            this.connect(index, this.rowColTransposed(row, col - 1));
-        }
-        if (col < this.n) {
-            this.connect(index, this.rowColTransposed(row, col + 1));
-        }
-    }
+		this.n = n;
+		this.connections = new WeightedQuickUnionUF(n*n+2);
+		this.top = 0;
+		this.bottom = n*n + 1;
+		this.sites[top] = true;
+		this.sites[bottom] = true;
+		this.openedSites = 0;
+	}
 
-    public boolean isOpen(int row, int col) {
-        int index = this.rowColTransposed(row, col);
-        return this.sites[index];
-    }
+	/**
+	 * Returns the index into the data structure for row and col indices
+	 *
+	 * @param row
+	 *            the row index
+	 * @param col
+	 *            the column index
+	 * @return the index into the data structure
+	 */
+	private int rowColTransposed(int row, int col) {
+		if (row <= 0 || row > this.n || col <= 0  || col > this.n) {
+			throw new IllegalArgumentException();
+		}
+		return (col) + this.n * (row - 1);
+	}
 
-    public boolean isFull(int row, int col) {
-        int index = this.rowColTransposed(row, col);
-        return this.connections.connected(index, this.top);
-    }
+	/**
+	 * Connects one index to another index
+	 *
+	 * @param index1
+	 *            the first index
+	 * @param index2
+	 *            the second index
+	 */
+	private void connect(int index1, int index2) {
+		if (this.sites[index1] && this.sites[index2]) {
+			this.connections.union(index1, index2);
+		}
+	}
 
-    public int numberOfOpenSites() {
-        return this.openedSites;
-    }
+	/**
+	 * Open the square in row <i> row </i> and column <i> col </i>
+	 *
+	 * @param row
+	 *            the row index
+	 * @param col
+	 *            the column index
+	 */
+	public void open(int row, int col) {
+		this.openedSites ++;
+		int index = this.rowColTransposed(row, col);
+		this.sites[index] = true;
 
-    public boolean percolates() {
-        return this.connections.connected(this.top, this.bottom);
-    }
+		// do up and above first
+		if (row == 1 ) {
+			this.connect(this.top, index);
+			this.connect(index, this.rowColTransposed(row + 1, col));
+		} else if (row == this.n) {
+			this.connect(this.bottom, index);
+			this.connect(index, this.rowColTransposed(row - 1, col));
+		} else {
+			this.connect(index, this.rowColTransposed(row - 1, col));
+			this.connect(index, this.rowColTransposed(row + 1, col));
+		}
 
-    public static void main(String[] args) {
-        Percolation percolation = new Percolation(3);
-        StdOut.print(percolation.percolates());
-        StdOut.println();
-        percolation.open(1, 1);
-        StdOut.print(percolation.percolates());
-        StdOut.println();
-        percolation.open(2, 2);
-        StdOut.print(percolation.percolates());
-        StdOut.println();
-        percolation.open(3, 3);
-        StdOut.print(percolation.percolates());
-        StdOut.println();
-        percolation.open(1, 2);
-        StdOut.print(percolation.percolates());
-        StdOut.println();
-        percolation.open(2, 3);
-        StdOut.print(percolation.percolates());
-        StdOut.println();
-    }
+		// if not in the first or last column
+		if (col > 1) {
+			this.connect(index, this.rowColTransposed(row, col - 1));
+		}
+		if (col < this.n) {
+			this.connect(index, this.rowColTransposed(row, col + 1));
+		}
+	}
+
+	/**
+	 * Returns whether the given cell is open
+	 *
+	 * @param row
+	 *            the row index
+	 * @param col
+	 *            the column index
+	 * @return true if cell is open, false otherwise
+	 */
+	public boolean isOpen(int row, int col) {
+		int index = this.rowColTransposed(row, col);
+		return this.sites[index];
+	}
+
+	/**
+	 * Returns whether the given cell is full (has a path to top row)
+	 *
+	 * @param row
+	 *            the row index
+	 * @param col
+	 *            the column index
+	 * @return true if cell is full, false otherwise
+	 */
+	public boolean isFull(int row, int col) {
+		int index = this.rowColTransposed(row, col);
+		return this.connections.connected(index, this.top);
+	}
+
+	/**
+	 * Returns the number of open sites
+	 *
+	 * @return the number of open sites
+	 */
+	public int numberOfOpenSites() {
+		return this.openedSites;
+	}
+
+	/**
+	 * Returns whether the structure percolates or not
+	 *
+	 * @return true if is percolates, false otherwise
+	 */
+	public boolean percolates() {
+		return this.connections.connected(this.top, this.bottom);
+	}
+
+	/**
+	 * Simple program for testing
+	 *
+	 * @param args
+	 *            - none used
+	 */
+	public static void main(String[] args) {
+		Percolation percolation = new Percolation(3);
+		StdOut.print(percolation.percolates());
+		StdOut.println();
+
+		// open up the top left square
+		percolation.open(1, 1);
+		StdOut.print(percolation.percolates());
+		StdOut.println();
+
+		// open the center square
+		percolation.open(2, 2);
+		StdOut.print(percolation.percolates());
+		StdOut.println();
+
+		// open bottom right square
+		percolation.open(3, 3);
+		StdOut.print(percolation.percolates());
+		StdOut.println();
+
+		// open another square
+		percolation.open(1, 2);
+		StdOut.print(percolation.percolates());
+		StdOut.println();
+
+		// should now perculate
+		percolation.open(2, 3);
+		StdOut.print(percolation.percolates());
+		StdOut.println();
+	}
 }
--- src/a1/PercolationStats.java
+++ src/a1/PercolationStats.java
@@ -1,8 +1,17 @@
+/******************************************************************************
+ *  Compilation:  javac -cp  "alg4s.jar" Percolation.java
+ *  Execution:    java PercolationStats
+ *  Dependencies: algs4
+ *  Author: Dallas Fraser
+ *  A program to test various statistics about percolation
+ *  For use on Coursera, Algorithms Part I programming assignment.
+ *
+ ******************************************************************************/
 package a1;
 
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.StdOut;
 
 /**
  * This class runs an experiment to determine p* for percolation
@@ -10,52 +19,98 @@ import edu.princeton.cs.algs4.StdOut;
  * @since 2018-06-06
  */
 public class PercolationStats {
-    private final double[] results;
-    private final int trials;
-    private final static double CONFIDENCE_95 = 1.96;
-    public PercolationStats(int n, int trials) {
-        if (trials <= 0 || n <= 0) {
-            throw new IllegalArgumentException();
-        }
-        this.results = new double[trials];
-        for (int trial = 0; trial < trials; trial++) {
-            this.results[trial] = this.runTrial(n);
-        }
-        this.trials = trials;
-    }
+	private final double[] results;
+	private final int trials;
+	private static final double CONFIDENCE_95 = 1.96;
 
-    private double runTrial(int n) {
-        Percolation percolation = new Percolation(n);
-        while (!percolation.percolates()) {
-            percolation.open(StdRandom.uniform(1, n+1), StdRandom.uniform(1, n+1));
-        }
-        return ((double) percolation.numberOfOpenSites() / (double) (n*n));
-    }
+	/**
+	 * The constructors that does x number of <i>trials</i> on a board size <i>n</i>
+	 *
+	 * @param n
+	 *            the size of the board
+	 * @param trials
+	 *            the number of trials
+	 */
+	public PercolationStats(int n, int trials) {
+		if (trials <= 0 || n <= 0) {
+			throw new IllegalArgumentException();
+		}
 
-    public double mean() {
-        return StdStats.mean(this.results);
-    }
+		// run the number of trials
+		this.results = new double[trials];
+		for (int trial = 0; trial < trials; trial++) {
+			this.results[trial] = this.runTrial(n);
+		}
+		this.trials = trials;
+	}
 
-    public double stddev() {
-        return StdStats.stddev(this.results);
-    }
+	/**
+	 * Performs on trial and Returns the number of open sites needed until it
+	 * percolates
+	 *
+	 * @param n
+	 *            the size of the structure
+	 * @return the number of open sites are a percentage
+	 */
+	private double runTrial(int n) {
+		Percolation percolation = new Percolation(n);
+		while (!percolation.percolates()) {
+			percolation.open(StdRandom.uniform(1, n+1), StdRandom.uniform(1, n+1));
+		}
+		return ((double) percolation.numberOfOpenSites() / (double) (n*n));
+	}
 
-    public double confidenceLo() {
-        return this.mean() - ((PercolationStats.CONFIDENCE_95 * this.stddev()) / this.trials);
-    }
+	/**
+	 * Returns the mean of the results
+	 *
+	 * @return the mean
+	 */
+	public double mean() {
+		return StdStats.mean(this.results);
+	}
 
-    public double confidenceHi() {
-        return this.mean() + ((PercolationStats.CONFIDENCE_95 * this.stddev()) / this.trials);
-    }
+	/**
+	 * Returns the standard deviation from the mean for the results
+	 *
+	 * @return the standard deviation
+	 */
+	public double stddev() {
+		return StdStats.stddev(this.results);
+	}
 
-    public static void main(String[] args) {
-        if (args.length != 2) {
-            StdOut.println("Not enough command line arguments: java-algs4 PercolationStats n T");
-        } else {
-            PercolationStats stats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-            StdOut.println("mean                    =" + stats.mean());
-            StdOut.println("stddev                  =" + stats.stddev());
-            StdOut.println("95% confidence interval =" + "[" + stats.confidenceLo() + ", " + stats.confidenceHi() + "]");
-        }
-    }
+	/**
+	 * Returns the lower end of the confidence interval
+	 *
+	 * @return lower end of the confidence interval
+	 */
+	public double confidenceLo() {
+		return this.mean() - ((PercolationStats.CONFIDENCE_95 * this.stddev()) / this.trials);
+	}
+
+	/**
+	 * Returns the upper end of the confidence interval
+	 *
+	 * @return upper end of the confidence interval
+	 */
+	public double confidenceHi() {
+		return this.mean() + ((PercolationStats.CONFIDENCE_95 * this.stddev()) / this.trials);
+	}
+
+	/**
+	 * Ouputs the mean, standard deviation and the 95% confidence interval
+	 * 
+	 * @param args
+	 *            (size, trials) the size of the structure, the number of trials to
+	 *            run
+	 */
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			StdOut.println("Not enough command line arguments: java-algs4 PercolationStats n T");
+		} else {
+			PercolationStats stats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+			StdOut.println("mean                    =" + stats.mean());
+			StdOut.println("stddev                  =" + stats.stddev());
+			StdOut.println("95% confidence interval =" + "[" + stats.confidenceLo() + ", " + stats.confidenceHi() + "]");
+		}
+	}
 }
