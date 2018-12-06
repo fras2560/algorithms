8683e5fdf8f76cf95646d9b8509558259814c003
fras2560 <dallas.fraser.waterloo@gmail.com>
2018-12-06 17:52:22 -0500

    Started doing checkstyle on older parts

--- src/a3/BruteCollinearPoints.java
+++ src/a3/BruteCollinearPoints.java
@@ -6,6 +6,7 @@
  *
  ******************************************************************************/
 package a3;
+
 import java.util.Arrays;
 
 import edu.princeton.cs.algs4.In;
@@ -19,197 +20,189 @@ import edu.princeton.cs.algs4.StdOut;
  *
  */
 public class BruteCollinearPoints {
-	private static final double EPSILON = 0.000000001;
-	private final LineSegment[] lines;
-	private final int numberSegments;
+  private static final double EPSILON = 0.000000001;
+  private final LineSegment[] lines;
+  private final int numberSegments;
 
-	/**
-	 * A constructor for finding the collinear points for the given points using a
-	 * brute force approach
-	 *
-	 * @param points
-	 *            the list of points
-	 */
-	public BruteCollinearPoints(Point[] points) {
+  /**
+   * A constructor for finding the collinear points for the given points using a
+   * brute force approach.
+   *
+   * @param points the list of points
+   */
+  public BruteCollinearPoints(Point[] points) {
 
-		// points cannot be null or any particular point
-		if (points == null || Arrays.asList(points).contains(null)) {
-			throw new IllegalArgumentException();
-		}
+    // points cannot be null or any particular point
+    if (points == null || Arrays.asList(points).contains(null)) {
+      throw new IllegalArgumentException();
+    }
 
-		// initialize some variables
-		double s1;
-		double s2;
-		double s3;
-		int segmentCount = 0;
-		LineSegment[] lineSegments = new LineSegment[points.length];
+    // initialize some variables
+    double s1;
+    double s2;
+    double s3;
+    int segmentCount = 0;
+    LineSegment[] lineSegments = new LineSegment[points.length];
 
-		// four loops to find all points that are collinear
-		for (int p1 = 0; p1 < points.length; p1++) {
-			for (int p2 = p1 + 1; p2 < points.length; p2++) {
-				s1 = this.computeSlope(points[p1], points[p2]);
-				for (int p3 = p2 + 1; p3 < points.length; p3++) {
-					s2 = this.computeSlope(points[p1], points[p3]);
-					for (int p4 = p3 + 1; p4 < points.length; p4++) {
-						s3 = this.computeSlope(points[p1], points[p4]);
+    // four loops to find all points that are collinear
+    for (int p1 = 0; p1 < points.length; p1++) {
+      for (int p2 = p1 + 1; p2 < points.length; p2++) {
+        s1 = this.computeSlope(points[p1], points[p2]);
+        for (int p3 = p2 + 1; p3 < points.length; p3++) {
+          s2 = this.computeSlope(points[p1], points[p3]);
+          for (int p4 = p3 + 1; p4 < points.length; p4++) {
+            s3 = this.computeSlope(points[p1], points[p4]);
 
-						// compare the floating point to lines closer than EPILON or equal (vertical
-						// lines)
-						if ((Math.abs(s1 - s2) < EPSILON && Math.abs(s1 - s3) < EPSILON) || (s1 == s2 && s1 == s3)) {
-							if (segmentCount == lineSegments.length) {
-								lineSegments = BruteCollinearPoints.resize(lineSegments, segmentCount * 2);
-							}
-							lineSegments[segmentCount] = new LineSegment(
-									BruteCollinearPoints.smallestPoint(points[p1], points[p2], points[p3], points[p4]),
-									BruteCollinearPoints.largestPoint(points[p1], points[p2], points[p3], points[p4]));
-							segmentCount += 1;
-						}
-					}
-				}
-			}
-		}
-		this.lines = BruteCollinearPoints.resize(lineSegments, segmentCount);
-		this.numberSegments = segmentCount;
-	}
+            // compare the floating point to lines closer than EPILON or equal (vertical
+            // lines)
+            if ((Math.abs(s1 - s2) < EPSILON && Math.abs(s1 - s3) < EPSILON)
+                || (s1 == s2 && s1 == s3)) {
+              if (segmentCount == lineSegments.length) {
+                lineSegments = BruteCollinearPoints.resize(lineSegments, segmentCount * 2);
+              }
+              lineSegments[segmentCount] = new LineSegment(
+                  BruteCollinearPoints.smallestPoint(points[p1],
+                      points[p2], points[p3], points[p4]),
+                  BruteCollinearPoints.largestPoint(points[p1],
+                      points[p2], points[p3], points[p4]));
+              segmentCount += 1;
+            }
+          }
+        }
+      }
+    }
+    this.lines = BruteCollinearPoints.resize(lineSegments, segmentCount);
+    this.numberSegments = segmentCount;
+  }
 
-	private double computeSlope(Point p1, Point p2) {
-		if (p1 == null || p2 == null) {
-			throw new IllegalArgumentException();
-		}
-		double slope = p1.slopeTo(p2);
-		if (slope == Double.NEGATIVE_INFINITY) {
-			throw new IllegalArgumentException();
-		}
-		return slope;
-	}
-	/**
-	 * Returns the number of line segments
-	 *
-	 * @return the number of segments
-	 */
-	public int numberOfSegments() {
-		return this.numberSegments;
-	}
+  private double computeSlope(Point p1, Point p2) {
+    if (p1 == null || p2 == null) {
+      throw new IllegalArgumentException();
+    }
+    double slope = p1.slopeTo(p2);
+    if (slope == Double.NEGATIVE_INFINITY) {
+      throw new IllegalArgumentException();
+    }
+    return slope;
+  }
 
-	/**
-	 * Returns the line segments
-	 *
-	 * @return an array copy of the line segments
-	 */
-	public LineSegment[] segments() {
-		return Arrays.copyOf(this.lines, this.lines.length);
-	}
+  /**
+   * Returns the number of line segments.
+   *
+   * @return the number of segments
+   */
+  public int numberOfSegments() {
+    return this.numberSegments;
+  }
 
-	/**
-	 * Returns the smallest point of the four given points
-	 *
-	 * @param p1
-	 *            a point
-	 * @param p2
-	 *            a point
-	 * @param p3
-	 *            a point
-	 * @param p4
-	 *            a point
-	 * @return the smallest point
-	 */
-	private static Point smallestPoint(Point p1, Point p2, Point p3, Point p4) {
-		Point smallest;
-		if (p1.compareTo(p2) <= 0 && p1.compareTo(p3) <= 0 && p1.compareTo(p4) <= 0) {
-			smallest = p1;
-		} else if (p2.compareTo(p3) <= 0 && p2.compareTo(p4) <= 0) {
-			smallest = p2;
-		} else if (p3.compareTo(p4) <= 0) {
-			smallest = p3;
-		} else {
-			smallest = p4;
-		}
-		return smallest;
-	}
+  /**
+   * Returns the line segments.
+   *
+   * @return an array copy of the line segments
+   */
+  public LineSegment[] segments() {
+    return Arrays.copyOf(this.lines, this.lines.length);
+  }
 
-	/**
-	 * Returns the largest point of the four given points
-	 *
-	 * @param p1
-	 *            a point
-	 * @param p2
-	 *            a point
-	 * @param p3
-	 *            a point
-	 * @param p4
-	 *            a point
-	 * @return the largest point
-	 */
-	private static Point largestPoint(Point p1, Point p2, Point p3, Point p4) {
-		Point largest;
-		if (p1.compareTo(p2) >= 0 && p1.compareTo(p3) >= 0 && p1.compareTo(p4) >= 0) {
-			largest = p1;
-		} else if (p2.compareTo(p3) >= 0 && p2.compareTo(p4) >= 0) {
-			largest = p2;
-		} else if (p3.compareTo(p4) >= 0) {
-			largest = p3;
-		} else {
-			largest = p4;
-		}
-		return largest;
-	}
+  /**
+   * Returns the smallest point of the four given points.
+   *
+   * @param p1 a point
+   * @param p2 a point
+   * @param p3 a point
+   * @param p4 a point
+   * @return the smallest point
+   */
+  private static Point smallestPoint(Point p1, Point p2, Point p3, Point p4) {
+    Point smallest;
+    if (p1.compareTo(p2) <= 0 && p1.compareTo(p3) <= 0 && p1.compareTo(p4) <= 0) {
+      smallest = p1;
+    } else if (p2.compareTo(p3) <= 0 && p2.compareTo(p4) <= 0) {
+      smallest = p2;
+    } else if (p3.compareTo(p4) <= 0) {
+      smallest = p3;
+    } else {
+      smallest = p4;
+    }
+    return smallest;
+  }
 
-	/**
-	 * Returns a resized segment
-	 *
-	 * @param array
-	 *            the array of lines
-	 * @param capacity
-	 *            the new capacity to resize to
-	 * @return a resized array of line segments
-	 */
-	private static LineSegment[] resize(LineSegment[] array, int capacity) {
-		LineSegment[] copy = new LineSegment[capacity];
-		int pos = 0;
-		int copyPos = 0;
-		while (pos < array.length) {
-			if (array[pos] != null) {
-				copy[copyPos] = array[pos];
-				copyPos++;
-			}
-			pos++;
-		}
-		return copy;
-	}
+  /**
+   * Returns the largest point of the four given points.
+   *
+   * @param p1 a point
+   * @param p2 a point
+   * @param p3 a point
+   * @param p4 a point
+   * @return the largest point
+   */
+  private static Point largestPoint(Point p1, Point p2, Point p3, Point p4) {
+    Point largest;
+    if (p1.compareTo(p2) >= 0 && p1.compareTo(p3) >= 0 && p1.compareTo(p4) >= 0) {
+      largest = p1;
+    } else if (p2.compareTo(p3) >= 0 && p2.compareTo(p4) >= 0) {
+      largest = p2;
+    } else if (p3.compareTo(p4) >= 0) {
+      largest = p3;
+    } else {
+      largest = p4;
+    }
+    return largest;
+  }
 
-	/**
-	 * The main for testing
-	 *
-	 * @param args
-	 *            n the number of points
-	 */
-	public static void main(String[] args) {
+  /**
+   * Returns a resized segment.
+   *
+   * @param array    the array of lines
+   * @param capacity the new capacity to resize to
+   * @return a resized array of line segments
+   */
+  private static LineSegment[] resize(LineSegment[] array, int capacity) {
+    LineSegment[] copy = new LineSegment[capacity];
+    int pos = 0;
+    int copyPos = 0;
+    while (pos < array.length) {
+      if (array[pos] != null) {
+        copy[copyPos] = array[pos];
+        copyPos++;
+      }
+      pos++;
+    }
+    return copy;
+  }
 
-		// read the n points from a file
-		In in = new In(args[0]);
-		int n = in.readInt();
-		Point[] points = new Point[n];
-		for (int i = 0; i < n; i++) {
-			int x = in.readInt();
-			int y = in.readInt();
-			points[i] = new Point(x, y);
-		}
+  /**
+   * The main for testing.
+   *
+   * @param args n the number of points
+   */
+  public static void main(String[] args) {
 
-		// draw the points
-		StdDraw.enableDoubleBuffering();
-		StdDraw.setXscale(0, 32768);
-		StdDraw.setYscale(0, 32768);
-		for (Point p : points) {
-			p.draw();
-		}
-		StdDraw.show();
+    // read the n points from a file
+    In in = new In(args[0]);
+    int n = in.readInt();
+    Point[] points = new Point[n];
+    for (int i = 0; i < n; i++) {
+      int x = in.readInt();
+      int y = in.readInt();
+      points[i] = new Point(x, y);
+    }
 
-		// print and draw the line segments
-		BruteCollinearPoints collinear = new BruteCollinearPoints(points);
-		for (LineSegment segment : collinear.segments()) {
-			StdOut.println(segment);
-			segment.draw();
-		}
-		StdDraw.show();
-	}
+    // draw the points
+    StdDraw.enableDoubleBuffering();
+    StdDraw.setXscale(0, 32768);
+    StdDraw.setYscale(0, 32768);
+    for (Point p : points) {
+      p.draw();
+    }
+    StdDraw.show();
+
+    // print and draw the line segments
+    BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+    for (LineSegment segment : collinear.segments()) {
+      StdOut.println(segment);
+      segment.draw();
+    }
+    StdDraw.show();
+  }
 }
--- src/a3/FastCollinearPoints.java
+++ src/a3/FastCollinearPoints.java
@@ -6,6 +6,7 @@
  *
  ******************************************************************************/
 package a3;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -18,207 +19,198 @@ import edu.princeton.cs.algs4.StdOut;
 
 /**
  * A Class for implementing a faster approach using sorting to finding collinear
- * points
+ * points.
  *
  * @author dfraser
  *
  */
 public class FastCollinearPoints {
-	private static final double EPSILON = 0.000000001;
-	private final LineSegment[] lines;
-	private final int numberSegments;
+  private static final double EPSILON = 0.000000001;
+  private final LineSegment[] lines;
+  private final int numberSegments;
 
-	/**
-	 * A constructor for finding the collinear points for the given points using a
-	 * faster approach by sorting the points
-	 *
-	 * @param points
-	 *            the list of points
-	 */
-	public FastCollinearPoints(Point[] points) {
+  /**
+   * A constructor for finding the collinear points for the given points using a
+   * faster approach by sorting the points.
+   *
+   * @param points the list of points
+   */
+  public FastCollinearPoints(Point[] points) {
 
-		// points cannot be null or any particular point
-		if (points == null || Arrays.asList(points).contains(null)) {
-			throw new IllegalArgumentException();
-		}
+    // points cannot be null or any particular point
+    if (points == null || Arrays.asList(points).contains(null)) {
+      throw new IllegalArgumentException();
+    }
 
-		List<Point> pointList = new ArrayList<>(Arrays.asList(points));
-		List<Point[]> tempLines = new ArrayList<>();
-		while (!pointList.isEmpty()) {
-			Point origin = pointList.remove(0);
-			Collections.sort(pointList, origin.slopeOrder());
-			this.pullLines(tempLines, origin, pointList);
-		}
-		this.lines = createLines(tempLines);
-		this.numberSegments = tempLines.size();
-	}
+    List<Point> pointList = new ArrayList<>(Arrays.asList(points));
+    List<Point[]> tempLines = new ArrayList<>();
+    while (!pointList.isEmpty()) {
+      Point origin = pointList.remove(0);
+      Collections.sort(pointList, origin.slopeOrder());
+      this.pullLines(tempLines, origin, pointList);
+    }
+    this.lines = createLines(tempLines);
+    this.numberSegments = tempLines.size();
+  }
 
-	/**
-	 * Creates the line segment array from the given points
-	 *
-	 * @param lineSegments
-	 *            a list of two points forming a line
-	 * @return an array of line segments
-	 */
-	private static LineSegment[] createLines(List<Point[]> lineSegments) {
-		LineSegment[] createdLines = new LineSegment[lineSegments.size()];
-		for (int i = 0; i < lineSegments.size(); i++) {
-			createdLines[i] = new LineSegment(lineSegments.get(i)[0], lineSegments.get(i)[1]);
-		}
-		return createdLines;
-	}
-	/**
-	 * Pulls the collinear lines and adds them to the given lines list
-	 *
-	 * @param lines
-	 *            a list of lines
-	 * @param origin
-	 *            the origin point
-	 * @param points
-	 *            a list of points
-	 */
-	private void pullLines(List<Point[]> lineSegments, Point origin, List<Point> points) {
+  /**
+   * Creates the line segment array from the given points.
+   *
+   * @param lineSegments a list of two points forming a line
+   * @return an array of line segments
+   */
+  private static LineSegment[] createLines(List<Point[]> lineSegments) {
+    LineSegment[] createdLines = new LineSegment[lineSegments.size()];
+    for (int i = 0; i < lineSegments.size(); i++) {
+      createdLines[i] = new LineSegment(lineSegments.get(i)[0], lineSegments.get(i)[1]);
+    }
+    return createdLines;
+  }
 
-		// initialize some values
-		int count;
-		double prevSlope;
-		Point current = null;
-		Point smallest;
-		Point largest;
+  /**
+   * Pulls the collinear lines and adds them to the given lines list.
+   *
+   * @param lines  a list of lines
+   * @param origin the origin point
+   * @param points a list of points
+   */
+  private void pullLines(List<Point[]> lineSegments, Point origin, List<Point> points) {
 
-		// get an iterator and init current to the first element
-		Iterator<Point> point = points.iterator();
-		if (!point.hasNext()) {
-			return;
-		}
-		current = nextPoint(point);
-		while (current != null) {
+    // initialize some values
+    int count;
+    double prevSlope;
+    Point current = null;
+    Point smallest;
+    Point largest;
 
-			// set the largest and smallest, and count and previous slope
-			count = 0;
-			prevSlope = origin.slopeTo(current);
-			smallest = origin.compareTo(current) <= 0 ? origin : current;
-			largest = origin.compareTo(current) >= 0 ? origin : current;
+    // get an iterator and init current to the first element
+    Iterator<Point> point = points.iterator();
+    if (!point.hasNext()) {
+      return;
+    }
+    current = nextPoint(point);
+    while (current != null) {
 
-			// see how many points in a row have the same slope to the origin
-			while (current != null && FastCollinearPoints.sameSlope(prevSlope, origin.slopeTo(current))) {
-				largest = largest.compareTo(current) >= 0 ? largest : current;
-				smallest = smallest.compareTo(current) <= 0 ? smallest : current;
-				count += 1;
-				current = nextPoint(point);
-			}
+      // set the largest and smallest, and count and previous slope
+      count = 0;
+      prevSlope = origin.slopeTo(current);
+      smallest = origin.compareTo(current) <= 0 ? origin : current;
+      largest = origin.compareTo(current) >= 0 ? origin : current;
 
-			Point[] line = new Point[] { smallest, largest };
-			// check to see if have enough points and is an unique line
-			if (count >= 3 && FastCollinearPoints.uniqueLineSegment(lineSegments, line)) {
-				lineSegments.add(line);
-			}
-		}
-	}
+      // see how many points in a row have the same slope to the origin
+      while (current != null && FastCollinearPoints.sameSlope(prevSlope, origin.slopeTo(current))) {
+        largest = largest.compareTo(current) >= 0 ? largest : current;
+        smallest = smallest.compareTo(current) <= 0 ? smallest : current;
+        count += 1;
+        current = nextPoint(point);
+      }
 
-	/**
-	 * Returns the number of line segments
-	 *
-	 * @return the number of segments
-	 */
-	public int numberOfSegments() {
-		return this.numberSegments;
-	}
+      Point[] line = new Point[] { smallest, largest };
+      // check to see if have enough points and is an unique line
+      if (count >= 3 && FastCollinearPoints.uniqueLineSegment(lineSegments, line)) {
+        lineSegments.add(line);
+      }
+    }
+  }
 
-	/**
-	 * Returns whether the two given slopes are the same
-	 *
-	 * @param s1
-	 *            the first slope
-	 * @param s2
-	 *            the second slope
-	 * @return true if slopes are considered the same, false otherwise
-	 */
-	private static boolean sameSlope(double s1, double s2) {
-		if (s1 == Double.NEGATIVE_INFINITY || s2 == Double.NEGATIVE_INFINITY) {
-			throw new IllegalArgumentException();
-		}
-		return Math.abs(s1 - s2) < EPSILON || s1 == s2;
-	}
+  /**
+   * Returns the number of line segments.
+   *
+   * @return the number of segments
+   */
+  public int numberOfSegments() {
+    return this.numberSegments;
+  }
 
-	/**
-	 * Returns the next point or null if no point is available
-	 *
-	 * @param points
-	 *            the points iterator
-	 * @return Point or null
-	 */
-	private static Point nextPoint(Iterator<Point> points) {
-		if (points.hasNext()) {
-			return points.next();
-		}
-		return null;
-	}
+  /**
+   * Returns whether the two given slopes are the same.
+   *
+   * @param s1 the first slope
+   * @param s2 the second slope
+   * @return true if slopes are considered the same, false otherwise
+   */
+  private static boolean sameSlope(double s1, double s2) {
+    if (s1 == Double.NEGATIVE_INFINITY || s2 == Double.NEGATIVE_INFINITY) {
+      throw new IllegalArgumentException();
+    }
+    return Math.abs(s1 - s2) < EPSILON || s1 == s2;
+  }
 
-	/**
-	 * Returns the line segments
-	 *
-	 * @return an array copy of the line segments
-	 */
-	public LineSegment[] segments() {
-		return Arrays.copyOf(this.lines, this.lines.length);
-	}
+  /**
+   * Returns the next point or null if no point is available.
+   *
+   * @param points the points iterator
+   * @return Point or null
+   */
+  private static Point nextPoint(Iterator<Point> points) {
+    if (points.hasNext()) {
+      return points.next();
+    }
+    return null;
+  }
 
-	/**
-	 * Check to see if the line segment is unique and not already added yet
-	 *
-	 * @param lines
-	 *            the list of lines
-	 * @param lineCheck
-	 *            the line to check
-	 * @return true if unique, false otherwise
-	 */
-	private static boolean uniqueLineSegment(List<Point[]> lineSegments, Point[] lineCheck) {
-		for (Point[] linePoints : lineSegments) {
-			if ((linePoints[0].compareTo(lineCheck[0]) == 0 && linePoints[1].compareTo(lineCheck[1]) == 0)
-					|| (linePoints[0].compareTo(lineCheck[0]) == 0
-					&& sameSlope(linePoints[0].slopeTo(lineCheck[1]), linePoints[0].slopeTo(linePoints[1])))
-					|| (linePoints[1].compareTo(lineCheck[1]) == 0
-					&& sameSlope(linePoints[1].slopeTo(lineCheck[0]), linePoints[1].slopeTo(linePoints[0])))) {
-				// line are identical
-				return false;
-			}
-		}
-		return true;
-	}
-	/**
-	 * The main for testing
-	 *
-	 * @param args
-	 *            n the number of points
-	 */
-	public static void main(String[] args) {
+  /**
+   * Returns the line segments.
+   *
+   * @return an array copy of the line segments
+   */
+  public LineSegment[] segments() {
+    return Arrays.copyOf(this.lines, this.lines.length);
+  }
 
-		// read the n points from a file
-		In in = new In(args[0]);
-		int n = in.readInt();
-		Point[] points = new Point[n];
-		for (int i = 0; i < n; i++) {
-			int x = in.readInt();
-			int y = in.readInt();
-			points[i] = new Point(x, y);
-		}
+  /**
+   * Check to see if the line segment is unique and not already added yet.
+   *
+   * @param lines     the list of lines
+   * @param lineCheck the line to check
+   * @return true if unique, false otherwise
+   */
+  private static boolean uniqueLineSegment(List<Point[]> lineSegments, Point[] lineCheck) {
+    for (Point[] linePoints : lineSegments) {
+      if ((linePoints[0].compareTo(lineCheck[0]) == 0 && linePoints[1].compareTo(lineCheck[1]) == 0)
+          || (linePoints[0].compareTo(lineCheck[0]) == 0
+              && sameSlope(linePoints[0].slopeTo(lineCheck[1]), linePoints[0].slopeTo(linePoints[1])))
+          || (linePoints[1].compareTo(lineCheck[1]) == 0
+              && sameSlope(linePoints[1].slopeTo(lineCheck[0]), linePoints[1].slopeTo(linePoints[0])))) {
+        // line are identical
+        return false;
+      }
+    }
+    return true;
+  }
 
-		// draw the points
-		StdDraw.enableDoubleBuffering();
-		StdDraw.setXscale(0, 32768);
-		StdDraw.setYscale(0, 32768);
-		for (Point p : points) {
-			p.draw();
-		}
-		StdDraw.show();
+  /**
+   * The main for testing.
+   *
+   * @param args n the number of points
+   */
+  public static void main(String[] args) {
 
-		// print and draw the line segments
-		FastCollinearPoints collinear = new FastCollinearPoints(points);
-		for (LineSegment segment : collinear.segments()) {
-			StdOut.println(segment);
-			segment.draw();
-		}
-		StdDraw.show();
-	}
+    // read the n points from a file
+    In in = new In(args[0]);
+    int n = in.readInt();
+    Point[] points = new Point[n];
+    for (int i = 0; i < n; i++) {
+      int x = in.readInt();
+      int y = in.readInt();
+      points[i] = new Point(x, y);
+    }
+
+    // draw the points
+    StdDraw.enableDoubleBuffering();
+    StdDraw.setXscale(0, 32768);
+    StdDraw.setYscale(0, 32768);
+    for (Point p : points) {
+      p.draw();
+    }
+    StdDraw.show();
+
+    // print and draw the line segments
+    FastCollinearPoints collinear = new FastCollinearPoints(points);
+    for (LineSegment segment : collinear.segments()) {
+      StdOut.println(segment);
+      segment.draw();
+    }
+    StdDraw.show();
+  }
 }
